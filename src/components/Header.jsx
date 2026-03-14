07e8b4b94aadc55cbf5079f2e2d6a77347c7c83f
sonamanayakkar <esonaman2@gmail.com>
2026-03-14 20:19:16 +0530

    customize position cd

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import './styles/header.css'
 import { Link } from 'react-router-dom'
 
-const Header = ({ dark ,color}) => {
+const Header = ({ dark, color }) => {
 
   let [bar, setBar] = useState(true);
 
@@ -11,9 +11,9 @@ const Header = ({ dark ,color}) => {
     setBar(!bar)
 
   }
-  
 
- 
+
+
 
 
   return (
@@ -27,19 +27,28 @@ const Header = ({ dark ,color}) => {
         <div className="two mb-lg-3 filter"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
           fill="currentColor" viewBox="0 0 24 24" > <path d="M8.5 1.5C6.92 1.5 5.6 2.56 5.16 4H2v2h3.16c.43 1.44 1.76 2.5 3.34 2.5s2.9-1.06 3.34-2.5H22V4H11.84A3.495 3.495 0 0 0 8.5 1.5m0 5C7.67 6.5 7 5.83 7 5s.67-1.5 1.5-1.5S10 4.17 10 5s-.67 1.5-1.5 1.5m0 9c-1.58 0-2.9 1.06-3.34 2.5H2v2h3.16c.43 1.44 1.76 2.5 3.34 2.5s2.9-1.06 3.34-2.5H22v-2H11.84a3.495 3.495 0 0 0-3.34-2.5m0 5c-.83 0-1.5-.67-1.5-1.5s.67-1.5 1.5-1.5 1.5.67 1.5 1.5-.67 1.5-1.5 1.5m8-12c-1.58 0-2.9 1.06-3.34 2.5H2v2h11.16c.43 1.44 1.76 2.5 3.34 2.5s2.9-1.06 3.34-2.5H22v-2h-2.16a3.495 3.495 0 0 0-3.34-2.5m0 5c-.83 0-1.5-.67-1.5-1.5s.67-1.5 1.5-1.5 1.5.67 1.5 1.5-.67 1.5-1.5 1.5"></path>
         </svg>
-        <div className="colors">
-          <div className="color yellow" onClick={()=>color("yellow")}></div>
-          <div className="color blue" onClick={()=>color("blue")}></div>
-          <div className="color green" onClick={()=>color("green")}></div>
-        </div>
-        
+          <div className="colors">
+            <p className='m-0'>Theme colors</p>
+
+            <div className="colss">
+              <div className="color yellow" onClick={() => color("yellow")}></div>
+              <div className="color blue" onClick={() => color("blue")}></div>
+              <div className="color green" onClick={() => color("green")}></div>
+            </div>
+
+          </div>
+
         </div>
 
         <div className="two" onClick={dark[0]}><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
           fill="currentColor" viewBox="0 0 24 24" style={dark[1] ? { display: "block" } : { display: "none" }}>
           <path d="M12 17.01c2.76 0 5.01-2.25 5.01-5.01S14.76 6.99 12 6.99 6.99 9.24 6.99 12s2.25 5.01 5.01 5.01M12 9c1.66 0 3.01 1.35 3.01 3.01s-1.35 3.01-3.01 3.01-3.01-1.35-3.01-3.01S10.34 9 12 9m1 10h-2v3h2zm0-17h-2v3h2zM2 11h3v2H2zm17 0h3v2h-3zM4.22 18.36l.71.71.71.71 1.06-1.06 1.06-1.06-.71-.71-.71-.71-1.06 1.06zM19.78 5.64l-.71-.71-.71-.71-1.06 1.06-1.06 1.06.71.71.71.71 1.06-1.06zm-12.02.7L6.7 5.28 5.64 4.22l-.71.71-.71.71L5.28 6.7l1.06 1.06.71-.71zm8.48 11.32 1.06 1.06 1.06 1.06.71-.71.71-.71-1.06-1.06-1.06-1.06-.71.71z" ></path>
         </svg>
-          <i className="fa-regular fa-moon" style={dark[1] ? { display: "none" } : { display: "block" }}></i>
+
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+            fill="currentColor" viewBox="0 0 24 24" style={dark[1] ? { display: "none" } : { display: "block" }}>
+            <path d="M20.71 13.51c-.78.23-1.58.35-2.38.35-4.52 0-8.2-3.68-8.2-8.2 0-.8.12-1.6.35-2.38.11-.35.01-.74-.25-1s-.64-.36-1-.25A10.17 10.17 0 0 0 2 11.8C2 17.42 6.57 22 12.2 22c4.53 0 8.45-2.91 9.76-7.24.11-.35.01-.74-.25-1s-.64-.36-1-.25M12.2 20C7.68 20 4 16.32 4 11.8a8.15 8.15 0 0 1 4.18-7.15c-.03.34-.05.68-.05 1.02 0 5.62 4.57 10.2 10.2 10.2.34 0 .68-.02 1.02-.05C17.93 18.38 15.23 20 12.2 20M16 8l.94-2.06L19 5l-2.06-.94L16 2l-.94 2.06L13 5l2.06.94zm4.25-.5-.55 1.2-1.2.55 1.2.55.55 1.2.55-1.2 1.2-.55-1.2-.55z"></path>
+          </svg>
         </div>
       </div>
       <div className={bar ? "navs d-flex flex-column gap-2 m-0" : " navs navscome d-flex flex-column gap-2 m-0"}>
